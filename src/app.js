503751ac9404519030fe304c3765a4c93f1f7dde
Benyamin Shafabakhsh <bsh@itacs.de>
2019-09-09 13:21:38 +0200

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 window.onload = function () {
+  console.log("start");
   //We instantiate our model
   const model = new DinnerModel();
+
+  const container = document.getElementsByClassName("page-content")[0]
+  const view = new OverviewView(container, model);
+  view.render()
+
   /**
    * IMPORTANT: app.js is the only place where you are allowed to
    * query for elements in the whole document.

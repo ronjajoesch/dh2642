34ecf7b94e8ab8fd06fb1bb83f0ac4173c6a6c88
Benyamin Shafabakhsh <bsh@itacs.de>
2019-09-11 17:55:40 +0200

    dynamic view

--- src/view/searchView.js
+++ src/view/searchView.js
@@ -17,7 +17,8 @@ class SearchView {
 
 
     render() {
-
+      var menu = this.model.getFullMenu();
+      console.log('THE TEST IS',menu);
         const sideMenu = this.container.querySelector(".row").appendChild(document.createElement('div'));
         sideMenu.className = "col-xs-12 col-sm-5 col-md-5 col-lg-4";
         const heading = sideMenu.appendChild(document.createElement('h4'));
@@ -34,36 +35,66 @@ class SearchView {
         // select.value = this.model.nGuest;
 
         people.innerHTML = select;
-
         const tableDiv = sideMenu.appendChild(document.createElement('div'));
+        const table = tableDiv.appendChild(document.createElement("table"));
+        table.className = "table";
+        const thead = table.appendChild(document.createElement("thead"));
+        const tr1 = thead.appendChild(document.createElement("tr"));     
+        const th1 = tr1.appendChild(document.createElement("th"));   
+        th1.innerText="Dish Name";
+        const th2 = tr1.appendChild(document.createElement("th"));
+        th2.innerText = "Cost";
+        
+        menu[0].forEach(el => {
+          
+          const tbody = table.appendChild(document.createElement("tbody"));
+          tbody.setAttribute("id","dishes");
+          const tr2 = tbody.appendChild(document.createElement("tr"));   
+          const td2 = tr2.appendChild(document.createElement("td"));
+          td2.className="value-starter-name";
+          td2.innerText=el.title;
+          const td3 = tr2.appendChild(document.createElement("td"));
+          td3.className="value-starter-value";
+          td3.innerText=el.pricePerServing;
+        });
 
-        tableDiv.innerHTML = `
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Dish Name</th>
-                <th >Cost</th>
-              </tr>
-            </thead>
-            <tbody id="dishes">
-              <tr>
-                <td class="value-starter-name">XXX</td>
-                <td class="value-starter-value">Kr 100</td>
-              </tr>
-              <tr>
-                <td class="value-main-course-name">YYY</td>
-                <td class="value-main-course-value">Kr 100</td>
-              </tr>
-              <tr>
-                <td class="value-dessert-name">ZZZ</td>
-                <td class="value-dessert-value">Kr 400</td>
-              </tr>
-              <tr>
-                <td></td>
-                <td class="value-total-price">Kr 400</td>
-              </tr>
-            </tbody>
-          </table>`;
+
+
+
+        // const tbody = table.appendChild("tbody");
+        // tbody.setAttribute = ('id','dishes');
+        // const tr3 = table.appendChild(document.createElement("tr"));
+        // const td1 = tr3.appendChild(document.createElement("td"));
+        // td1.innerText = "something";
+
+ 
+        // tableDiv.innerHTML = `
+        //   <table class="table">
+        //     <thead>
+        //       <tr>
+        //         <th>Dish Name</th>
+        //         <th >Cost</th>
+        //       </tr>
+        //     </thead>
+        //     <tbody id="dishes">
+        //       <tr>
+        //         <td class="value-starter-name">++</td>
+        //         <td class="value-starter-value">Kr 100</td>
+        //       </tr>
+        //       <tr>
+        //         <td class="value-main-course-name">YYY</td>
+        //         <td class="value-main-course-value">Kr 100</td>
+        //       </tr>
+        //       <tr>
+        //         <td class="value-dessert-name">ZZZ</td>
+        //         <td class="value-dessert-value">Kr 400</td>
+        //       </tr>
+        //       <tr>
+        //         <td></td>
+        //         <td class="value-total-price">Kr 400</td>
+        //       </tr>
+        //     </tbody>
+        //   </table>`;
 
 
       const buttonDiv = sideMenu.appendChild(document.createElement('div'));

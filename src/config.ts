6dbb50117faf8930ab16c75e82853fcfc4582587
Benyamin Shafabakhsh <bsh@itacs.de>
2019-09-26 17:18:14 +0200

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const API_KEY = '3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767';
+const Base_URL = '';
+export default{
+    API_KEY : API_KEY
+}
---
+++ src/services/sunsetService.ts
@@ -0,0 +1,11 @@
+import Promise, { config, resolve, reject } from 'bluebird';
+import Config from '../config';
+
+export class sunsetService{
+    public getAllDishes(dishType, query) : Promise<object>{
+        if(dishType == null || query == null){
+            
+        }
+        return null;
+    }
+}

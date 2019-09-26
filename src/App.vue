ae9e13eb0446d6d5efbd1621262925638c3307b3
Ronja Joesch <ronja.joesch@etudiant.univ-rennes1.fr>
2019-09-26 19:12:07 +0200

    add changes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header />
     <Article />
+    <Dish/>
   </div>
 </template>
 
 <script>
 import Header from './components/header/header.vue'
 import Article from './components/article/article.vue'
+import Dish from './components/selectDish/dish.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
-    Article
+    Article,
+    Dish,
   },
   data () {
     return {}
